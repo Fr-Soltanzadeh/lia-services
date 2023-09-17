e2b074a2dc235826f30a9713e58666f772701614
Farzaneh_sz <f.soltanzade72@gmail.com>
2023-09-17 17:25:36 +0330

    feat: base of routes

--- products/router.py
+++ products/router.py
@@ -1,4 +1,31 @@
 from fastapi import APIRouter, status, HTTPException
+from .schema import Product
+from typing import List, Any
 
 
 router = APIRouter(prefix="/products", tags=["Products"])
+
+
+@router.get("/", response_model=List[Product])
+def get_products() -> Any:
+    ...
+
+
+@router.get("/{product_id}", response_model=Product)
+def get_product_by_id(product_id: int) -> Any:
+    ...
+
+
+@router.post("/", response_model=Product)
+def post_product() -> Any:
+    ...
+
+
+@router.put("/{product_id}", response_model=Product)
+def change_product_by_id(product_id: int) -> Any:
+    ...
+
+
+@router.delete("/{product_id}")
+def delete_product_by_id(product_id: int) -> None:
+    ...

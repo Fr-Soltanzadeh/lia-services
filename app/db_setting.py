a68cf12cdd9a4bd45272f285e3b834eb58882e70
Farzaneh_sz <f.soltanzade72@gmail.com>
2023-09-19 12:23:41 +0330

    refactor: use .env for variables

--- app/db_setting.py
+++ app/db_setting.py
@@ -1,12 +1,11 @@
 from beanie import init_beanie
-import motor.motor_asyncio
+from motor.motor_asyncio import AsyncIOMotorClient
 
 from products.schema import Product
 
 
-async def init_db():
-    client = motor.motor_asyncio.AsyncIOMotorClient(
-        "mongodb://localhost:27017"
-    )
-
-    await init_beanie(database=client.test_db, document_models=[Product])
+async def init_db(db_name):
+    from config import mongodb_uri
+    
+    client = AsyncIOMotorClient(mongodb_uri)
+    await init_beanie(database=client[db_name], document_models=[Product])

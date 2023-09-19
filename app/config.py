2ea5c2ace747c1bd431ee5163cd45843c859abe9
Farzaneh_sz <f.soltanzade72@gmail.com>
2023-09-19 12:25:27 +0330

    refactor: using .env for variables

--- app/config.py
+++ app/config.py
@@ -1,15 +1,23 @@
 from fastapi import FastAPI
+from os import getenv
+from dotenv import load_dotenv
+
 from products.router import router as product_router
 from db_setting import init_db
+
 
 app = FastAPI()
 
 app.include_router(product_router)
 
+load_dotenv(dotenv_path=".env")
+hostname=getenv("hostname")
+port=int(getenv("port"))
+mongodb_uri=getenv("MongoDB_URI")
 
 @app.on_event("startup")
 async def start_db():
-    await init_db()
+    await init_db(getenv("database_name"))
 
 
 @app.get("/", tags=["Root"])
